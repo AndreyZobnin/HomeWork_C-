8aff13e167791a69279d905f9525a8c928e8529b
Andrey Zobnin <Andreyz3000@ya.ru>
2022-09-06 13:57:12 +0300

    Домашнее задание №3 к семинару 1

--- Seminar1Task3/Program.cs
+++ Seminar1Task3/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+//4 -> да
+//-3 -> нет
+//7 -> нет
+int a;
+Console.Write("введите число: ");
+a =  Convert.ToInt32(Console.ReadLine());
+if (a % 2==1) //формула определение четности числа Если ... и т.д.
+    {
+    Console.WriteLine("число нечетное");
+    }
+    else
+    {
+    Console.WriteLine("Число четное");
+    }
