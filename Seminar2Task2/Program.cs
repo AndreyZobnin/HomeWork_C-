f472b22d9882d4deab47c73597a9a15424be62cd
Andrey Zobnin <Andreyz3000@ya.ru>
2022-09-06 13:11:55 +0300

    Домашнее задание №2 к семинару 2

--- Seminar2Task2/Program.cs
+++ Seminar2Task2/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Write("Введи число: ");
+﻿//Напишите программу, которая выводит третью цифру случайно сгенерированного числа (от 1 до 65536) или сообщает, что третьей цифры нет.
+Console.Write("Введи число: ");
 int Number = Convert.ToInt32(Console.ReadLine());
 string NumberText = Convert.ToString(Number);
 if (NumberText.Length > 2){
