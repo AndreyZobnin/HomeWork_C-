c09b363850927b7c4dd6faa3d731578bf50c8cd7
Andrey Zobnin <Andreyz3000@ya.ru>
2022-09-12 19:35:04 +0300

    Домашнее задание №1 к семинару 7

--- Seminar7Task1/Program.cs
+++ Seminar7Task1/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.WriteLine("введите количество строк");
+﻿// Задача 1. Задайте двумерный массив размером m×n, заполненный случайными вещественными (тип double) числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+Console.WriteLine("введите количество строк");
 int linesVol = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите количество столбцов");
 int columnsVol = Convert.ToInt32(Console.ReadLine());
