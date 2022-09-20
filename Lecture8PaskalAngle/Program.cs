b2e1999ccba7cd07ca1abc8e40c1ab590a0ef48b
Andrey Zobnin <Andreyz3000@ya.ru>
2022-09-20 11:56:30 +0300

    Домашнее задание к лекции 8

--- Lecture8PaskalAngle/Program.cs
+++ Lecture8PaskalAngle/Program.cs
@@ -1,71 +1,34 @@
-﻿// ДЗ к лекции 8 треугольник паскаля
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-int n = InputRows("Введите количество строк : ");
-
-double[,] pascalAngle = new double[n + 1, 2 * n + 1];
-
-FullPascalAngle(pascalAngle);
-
-Console.WriteLine();
-PrintArray(pascalAngle);
-
-TransPascalAngle(pascalAngle);
-
-Console.WriteLine();
-PrintArray(pascalAngle);
-
-void TransPascalAngle(double[,] array)
+namespace PascalTriangle
 {
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    int count = 0;
-    for (int j = array.GetLength(1) - 1; j >= 0; j--)
+    class PascalTriangle
     {
-      if (array[i, j] != 0)
-      {
-        array[i, array.GetLength(1) / 2 + j - count] = array[i, j];
-        array[i, j] = 0;
-        count++;
-      }
-    }
-  }
-  array[array.GetLength(0) - 1, 0] = 1;
-}
+        static void Main(string[] args)
+        {
+            System.Console.Clear();
+            System.Console.Write("Введите количество строк: ");
+            int n = Convert.ToInt32(System.Console.ReadLine());
 
-void FullPascalAngle(double[,] pascalAngle)
-{
-  for (int k = 0; k < pascalAngle.GetLength(0); k++)
-  {
-    pascalAngle[k, 0] = 1;
-  }
-  for (int i = 1; i < pascalAngle.GetLength(0); i++)
-  {
-    for (int j = 1; j < i + 1; j++)
-    {
-      pascalAngle[i, j] = pascalAngle[i - 1, j] + pascalAngle[i - 1, j - 1];
-    }
-  }
-}
-int InputRows(string input)
-{
-  Console.Write(input);
-  int output = Convert.ToInt32(Console.ReadLine());
-  return output;
-}
+            for (int j = 0; j < n; j++)
+            {
+                int k = 1;
+                for (int space = 0; space < n - j; space++) System.Console.Write("  ");
 
-void PrintArray(double[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      if (array[i, j] != 0)
-      {
-          Console.Write($"{array[i, j]} ");
-      }
-      else Console.Write("  ");
-    }
-    Console.WriteLine();
-  }
-}
+                for (int i = 0; i <= j; i++)
+                {
+                    System.Console.Write("   {0:D} ", k);
+                    k = k * (j - i) / (i + 1);
+                }
+                System.Console.WriteLine();
+                System.Console.WriteLine();
+            }
+            System.Console.WriteLine();
+            System.Console.WriteLine();
+        }
 
+    }
+
+}
